3a4eabe6a4da543297786da4bf6d406b5107842d
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-11-27 11:12:44 +0200

    report, duplicates removed

--- semester-assignment/clojure/src/anxmatrix.clj
+++ semester-assignment/clojure/src/anxmatrix.clj
@@ -12,7 +12,7 @@
           (recur (rest l) r (conj out x) inv)
           (recur l (rest r) (conj out y) (+ inv (count (filter #(> % y) l)))))))))
 
-(defn anxmatrix.core/count-inv
+(defn- anxmatrix.core/count-inv
   ([coll] (count-inv coll 0))
   ([coll n]
    (if (> (count coll) 1)
@@ -24,13 +24,15 @@
 
 (defn anxmatrix.core/transpose [s] (apply mapv vector s))
 
-(defn anxmatrix.core/nested-for [f x y] (mapv (fn [a] (mapv (fn [b] (f a b)) y)) x))
+(defn- anxmatrix.core/nested-for [f x y] (mapv (fn [a] (mapv (fn [b] (f a b)) y)) x))
 
 (defn anxmatrix.core/matrix-mult [a b] (nested-for (fn [x y] (reduce + (map * x y))) a (transpose b)))
 
 (defn anxmatrix.core/matrix-add [a b] (mapv (fn [ai bi] (mapv + ai bi)) a b))
 
 (defn anxmatrix.core/matrix-substract [a b] (mapv (fn [ai bi] (mapv - ai bi)) a b))
+
+(defn anxmatrix.core/matrix-eq [ma mb] (and (= (count ma) (count mb)) (reduce #(and %1 %2) (map = ma mb))))
 
 (defn anxmatrix.core/matrix? [x]
   (true?
@@ -58,9 +60,7 @@
 
 (defn anxmatrix.core/row-count [x] (if-not (matrix? x) (throw (UnsupportedOperationException. "row-count not supported on this type")) (cc/count x)))
 (defn anxmatrix.core/column-count [x] (if-not (matrix? x) (throw (UnsupportedOperationException. "column-count not supported on this type")) (cc/count (get x 0))))
-(defn anxmatrix.core/square-mat [n e]
-  (let [repeater #(repeat n %)]
-    (matrix (-> e repeater repeater))))
+(defn anxmatrix.core/square-mat [n e] (let [repeater #(repeat n %)] (matrix (-> e repeater repeater))))
 
 (defn anxmatrix.core/identity-matrix [n]
   (let [row (conj (repeat (dec n) 0) 1)]
@@ -70,7 +70,5 @@
 (defn anxmatrix.core/get [m r c] (if-not (matrix? m) (throw (UnsupportedOperationException. "anxmatrix.core/get not supported on this type")) (cc/get (cc/get m r) c)))
 
 (defn anxmatrix.core/set [m r c v] (if-not (matrix? m) (throw (UnsupportedOperationException. "anxmatrix.core/set not supported on this type")) (assoc m r (assoc (cc/get m r) c v))))
-
-(defn anxmatrix.core/add [ma mb] (mapv (fn [x y] (mapv + x y)) ma mb))
 
 (defn anxmatrix.core/det [m] (if-not (and (matrix? m) (= (row-count m) (column-count m))) (throw (UnsupportedOperationException. "anxmatrix.core/det not supported on this type"))) (let [o (range (row-count m))] (reduce + (map (fn [p] (* (Math/pow -1 (cc/get (count-inv p) :inv)) (reduce * (map (fn [r c] (get m r c)) o p)))) (combo/permutations o)))))
